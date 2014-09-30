09a7f50ea973b05e6cd492a298473ff1e564b727
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2014-09-30 12:29:08 -0700

    Added apiFactory service to the home controller.

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,10 +1,17 @@
 angular.module('Defined')
-    .controller('HomeCtrl', function ($scope) {
-      $scope.list = [];
-      $scope.submit = function() {
-        if ($scope.text) {
-          $scope.list.push(this.text);
-          $scope.text = '';
-        }
-      };
-    });
+  .controller('HomeCtrl', ['$scope', 'apiFactory',
+    function ($scope, apiFactory) {
+
+
+
+
+
+
+    $scope.list = [];
+    $scope.submit = function() {
+      if ($scope.text) {
+        $scope.list.push(this.text);
+        $scope.text = '';
+      }
+    };
+  }]);
